d594bed642e0cce9a81943119b3a8f29dec8c4a5
matt2mi <2m1tema@gmail.com>
2020-04-14 10:44:04 +0200

    test env var heroku

--- server/server.js
+++ server/server.js
@@ -82,6 +82,7 @@ const port = process.env.PORT || 3000;
 const server = app.listen(port, () => {
   console.log(`Api on port ${port}`);
 });
+console.log('url', process.env.URL);
 
 // WEBSOCKETS
 // =============================================================================
