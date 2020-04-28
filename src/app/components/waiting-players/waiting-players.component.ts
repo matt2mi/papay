1ed72395c4cb4ef59ab59114bf1a3cf3bef43789
matt2mi <2m1tema@gmail.com>
2020-04-28 09:11:59 +0200

    suppression cachée de quelqu'un avant partie (faut cliquer sur le titre 'Liste des joueurs connectés') + refacto takeUntil (éviter multi subscribe)

--- src/app/components/waiting-players/waiting-players.component.ts
+++ src/app/components/waiting-players/waiting-players.component.ts
@@ -1,15 +1,18 @@
-import {Component, OnInit} from '@angular/core';
-import Player from '../../models/player';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {Router} from '@angular/router';
+import Player from '../../models/player';
 import {PlayersService} from '../../services/players.service';
+import {Subject} from 'rxjs';
+import {takeUntil} from 'rxjs/internal/operators';
 
 @Component({
   selector: 'app-waiting-players',
   templateUrl: './waiting-players.component.html',
   styleUrls: ['./waiting-players.component.scss']
 })
-export class WaitingPlayersComponent implements OnInit {
-
+export class WaitingPlayersComponent implements OnInit, OnDestroy {
+  showDelete = false;
+  private ngUnsubscribe = new Subject();
   errorMessage = '';
   currentPlayer: Player;
   connectedPlayers: Player[] = [];
@@ -24,11 +27,22 @@ export class WaitingPlayersComponent implements OnInit {
 
   initComponent() {
     this.currentPlayer = this.playersService.getCurrentPlayer();
-    this.playersService.newPlayers()
+    this.playersService.newPlayers().pipe(takeUntil(this.ngUnsubscribe))
       .subscribe((connectedPlayers: Player[]) => this.connectedPlayers = connectedPlayers);
-    this.playersService.getConnectedPlayers()
+    this.playersService.getConnectedPlayers().pipe(takeUntil(this.ngUnsubscribe))
       .subscribe((players: Player[]) => this.connectedPlayers = players);
-    this.playersService.partyStarted().subscribe(() => this.router.navigate(['playing']));
+    this.playersService.youAreKicked().pipe(takeUntil(this.ngUnsubscribe))
+      .subscribe((kickerName: string) => {
+        alert('Déso, t\'as été kické par ' + kickerName);
+        this.router.navigate(['login']);
+      });
+    this.playersService.playerKicked().pipe(takeUntil(this.ngUnsubscribe))
+      .subscribe((result: { kickedName: string, kickerName: string, players: Player[] }) => {
+        alert(result.kickedName + ' a été kické par ' + result.kickerName);
+        this.connectedPlayers = result.players;
+      });
+    this.playersService.partyStarted().pipe(takeUntil(this.ngUnsubscribe))
+      .subscribe(() => this.router.navigate(['playing']));
   }
 
   testFrontOnly() {
@@ -52,5 +66,17 @@ export class WaitingPlayersComponent implements OnInit {
           () => this.router.navigate(['playing']),
           (error: { error: boolean, message: string }) => this.errorMessage = error.message);
     }
+  }
+
+  deletePlayer(name: string) {
+    console.log('deletePlayer(' + name + ')');
+    this.playersService.deletePlayer(name).subscribe(
+      ({message}) => console.log('retour', message),
+      ({error}) => console.error('retour', error));
+  }
+
+  ngOnDestroy() {
+    this.ngUnsubscribe.next();
+    this.ngUnsubscribe.complete();
   }
 }
